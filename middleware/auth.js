c5fb6ad72e0711952620f93f65f7a50247502ffb
o.monastyrskyi <oleg88mo@gmail.com>
2019-11-12 17:20:55 +0200

    add security

--- middleware/auth.js
+++ middleware/auth.js
@@ -10,6 +10,7 @@ exports.protect = asyncHandler(async (req, res, next) => {
     if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
         token = req.headers.authorization.split(' ')[1];
     }
+    // use Token in cookie or use Auth header param
     // else if (req.cookies.token) {
     //     token = req.cookies.token
     // }
