b959f19febee40fed3cc6220a14ecd4ae5350ca3
o.monastyrskyi <oleg88mo@gmail.com>
2019-11-15 11:31:58 +0200

    log

--- server.js
+++ server.js
@@ -78,7 +78,7 @@ app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 const server = app.listen(PORT, console.log(`Server running in '${process.env.NODE_ENV}' mode. On port: ${PORT}`.blue.bold));
-
+console.log('RUN?')
 // Handle unhandled promise rejections
 process.on('unhandledRejection', (err, promise) => {
     console.log(`Error: ${err.message}`.red);
