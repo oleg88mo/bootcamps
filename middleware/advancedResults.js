a3dcefe4dcc3c65c8a40fcf258b322d5e10ec40a
o.monastyrskyi <oleg88mo@gmail.com>
2019-12-02 12:48:21 +0200

    search only by criteria

--- middleware/advancedResults.js
+++ middleware/advancedResults.js
@@ -1,85 +1,103 @@
 const advancedResults = (model, populate) => async (req, res, next) => {
     let query;
+    let valid = true;
     let reqQuery = {...req.query};
+    const queryPermission = ['select', 'sort', 'page', 'phone', 'website', 'name', 'limit', 'averageCost', 'averageRating', 'careers', 'housing', 'jobAssistance', 'jobGuarantee', 'acceptGi'];
 
-    // Fields to exclude
-    const removeFields = ['select', 'sort', 'page', 'limit'];
+    Object.keys(reqQuery).map(el => {
+        if (!queryPermission.includes(el)) {
+            return valid = false
+        }
+    });
 
-    // Loop over removeFields and delete them from reqQuery
-    removeFields.forEach(param => delete reqQuery[param]);
 
-    let queryStr = JSON.stringify(reqQuery);
+    if (valid) {
+        // Fields to exclude
+        const removeFields = ['select', 'sort', 'page', 'limit'];
 
-    queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, math => `$${math}`);
+        // Loop over removeFields and delete them from reqQuery
+        removeFields.forEach(param => delete reqQuery[param]);
 
-    if (req.query.name) {
-        // search by Name (startWith)
-        let regexp = new RegExp("^" + req.query.name.toLowerCase(), 'i');
+        let queryStr = JSON.stringify(reqQuery);
 
-        let filteredObj = JSON.parse(queryStr);
-        filteredObj.name = regexp;
+        queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, math => `$${math}`);
 
-        query = model.find(filteredObj);
-    }else{
-        query = model.find(JSON.parse(queryStr));
-    }
+        if (req.query.name) {
+            // search by Name (startWith)
+            let regexp = new RegExp("^" + req.query.name.toLowerCase(), 'i');
 
-    // Select Fields
-    if (req.query.select) {
-        const fields = req.query.select.split(',').join(' ');
-        query = query.select(fields);
-    }
+            let filteredObj = JSON.parse(queryStr);
+            filteredObj.name = regexp;
 
-    // Sort Fields
-    if (req.query.sort) {
-        const sortBy = req.query.sort.split(',').join(' ');
-        query = query.sort(sortBy);
+            query = model.find(filteredObj);
+        } else {
+            query = model.find(JSON.parse(queryStr));
+        }
+
+        // Select Fields
+        if (req.query.select) {
+            const fields = req.query.select.split(',').join(' ');
+            query = query.select(fields);
+        }
+
+        // Sort Fields
+        if (req.query.sort) {
+            const sortBy = req.query.sort.split(',').join(' ');
+            query = query.sort(sortBy);
+        } else {
+            query = query.sort('-createdAt');
+        }
+
+        // Pagination
+        const page = parseInt(req.query.page, 10) || 1;
+        const limit = parseInt(req.query.limit, 10) || 25;
+        const startIndex = (page - 1) * limit;
+        const endIndex = page * limit;
+        const total = await model.countDocuments();
+
+        query = query.skip(startIndex).limit(limit);
+
+        if (populate) {
+            query = query.populate(populate)
+        }
+
+        // Executing query
+        const results = await query;
+
+        // Pagination result
+        const pagination = {};
+
+        if (endIndex < total) {
+            pagination.next = {
+                page: page + 1,
+                limit
+            }
+        }
+
+        if (startIndex > 0) {
+            pagination.prev = {
+                page: page - 1,
+                limit
+            }
+        }
+
+        res.advancedResults = {
+            success: true,
+            count: results.length,
+            totalCount: total,
+            pagination,
+            data: results
+        };
+
+        next();
     } else {
-        query = query.sort('-createdAt');
+        res.advancedResults = {
+            success: false,
+            data: {}
+        };
+
+        next();
     }
-
-    // Pagination
-    const page = parseInt(req.query.page, 10) || 1;
-    const limit = parseInt(req.query.limit, 10) || 25;
-    const startIndex = (page - 1) * limit;
-    const endIndex = page * limit;
-    const total = await model.countDocuments();
-
-    query = query.skip(startIndex).limit(limit);
-
-    if (populate) {
-        query = query.populate(populate)
-    }
-
-    // Executing query
-    const results = await query;
-
-    // Pagination result
-    const pagination = {};
-
-    if (endIndex < total) {
-        pagination.next = {
-            page: page + 1,
-            limit
-        }
-    }
-
-    if (startIndex > 0) {
-        pagination.prev = {
-            page: page - 1,
-            limit
-        }
-    }
-
-    res.advancedResults = {
-        success: true,
-        count: results.length,
-        totalCount: total,
-        pagination,
-        data: results
-    };
-
-    next();
 };
 
 module.exports = advancedResults;
